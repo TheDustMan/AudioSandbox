5633e51b0b9b99a879b2cecf16309bcb5ee18d48
Dustin Aguilar <dustin@nugen.io>
2018-07-04 19:05:20 -0700

    Initial commit

---
+++ AudioSandbox/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour {
+
+    // un-optimized version
+    public double frequency = 440.0;
+    public double gain = 0.05;
+    private double increment;
+    private double phase;
+    private double sampling_frequency = 48000;
+
+    void OnAudioFilterRead(float[] data, int channels)
+    {
+        // update increment in case frequency has changed
+        increment = frequency * 2.0 * Mathf.PI / sampling_frequency;
+
+        for (var i = 0; i < data.Length; i += channels)
+        {
+            phase += increment;
+            // this is where we copy audio data to make them “available” to Unity
+            data[i] = (float)(gain * Mathf.Sin((float)phase));
+            // if we have stereo, we copy the mono data to each channel
+            if (channels == 2)
+            {
+                data[i + 1] = data[i];   
+            }
+            if (phase > (2.0 * Mathf.PI))
+            {
+                phase = 0.0;
+            }
+        }
+    }
+} 
---
+++ AudioSandbox/Assets/WaveSpawner.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    enum Note
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G
+    };
+
+    private Dictionary<KeyCode, GameObject> oscillators = new Dictionary< KeyCode, GameObject>();
+    private Dictionary<Note, double> noteToFrequency = new Dictionary<Note, double>();
+    private Dictionary<KeyCode, Note> keyToNote = new Dictionary<KeyCode, Note>();
+
+	// Use this for initialization
+    void Start () {
+        noteToFrequency.Add(Note.A, 220.0);
+        noteToFrequency.Add(Note.B, 246.94);
+        noteToFrequency.Add(Note.C, 261.63);
+        noteToFrequency.Add(Note.D, 293.66);
+        noteToFrequency.Add(Note.E, 329.63);
+        noteToFrequency.Add(Note.F, 349.23);
+        noteToFrequency.Add(Note.G, 392.00);
+
+        keyToNote.Add(KeyCode.A, Note.A);
+        keyToNote.Add(KeyCode.B, Note.B);
+        keyToNote.Add(KeyCode.C, Note.C);
+        keyToNote.Add(KeyCode.D, Note.D);
+        keyToNote.Add(KeyCode.E, Note.E);
+        keyToNote.Add(KeyCode.F, Note.F);
+        keyToNote.Add(KeyCode.G, Note.G);
+	}
+	
+    private void _generateOscillatorForKeyCode(KeyCode keyCode)
+    {
+        Note note = keyToNote[keyCode];
+
+        if (oscillators.ContainsKey(keyCode) &&
+                oscillators[keyCode] != null)
+        {
+            Destroy((GameObject)oscillators[keyCode]);
+            oscillators[keyCode] = null;
+        }
+
+        double octaveMultiplier = (double)Random.Range(1, 5);
+        double computedFrequency = noteToFrequency[note] * octaveMultiplier;
+
+        oscillators[keyCode] = (GameObject)Instantiate(Resources.Load("Prefabs/Oscillator"));
+        ((GameObject)oscillators[keyCode]).GetComponent<Oscillator>().frequency = computedFrequency;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _generateOscillatorForKeyCode(KeyCode.A);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            _generateOscillatorForKeyCode(KeyCode.B);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            _generateOscillatorForKeyCode(KeyCode.C);
+        }
+	}
+}
